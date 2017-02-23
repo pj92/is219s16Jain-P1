0b225373b07180a4c642c97425a94262541caeab
pj92 <pj92@njit.edu>
2017-02-23 02:24:18 -0500

    logo/navbar

--- index.php
+++ index.php
@@ -18,7 +18,20 @@
 </head>
 
 <body>
+  <!-- Navbar-->
+  <nav class="navbar navbar.inverse navbar.fixed.top" id="portfolio navbar">
+    <div class="container">
+      <div class="navbar.header">
+        <button type="button" class="navbar.toggle" data.toggle="collapse" data.target="#navbar.collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
 
+        <a href = ""><img class="navbar.brand" src = "imges/logo.png"></a>
+
+
+  </nav>
 
   <script scr="https://code.jquery.com/jquery-3.1.1.min.js"></script>
   <!-- Latest compiled and minified JavaScript -->
